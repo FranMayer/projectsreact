904ab40e99e3f790513262b970e6a74e546f55f9
Franco Mayer <francomayer96@outlook.com>
2025-01-21 15:23:12 -0300

    new component

--- projects/00-componentes/src/App.jsx
+++ projects/00-componentes/src/App.jsx
@@ -1,26 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { TwitterCard } from './TwitterCard';
+import './App.css';
+import { TwitterCard } from './xcard';
 
 function App() {
-
   return (
     <div className='App'>
-    <TwitterCard
-      avatar="https://unavatar.io/github/mdo"
-      name="Mark Otto"
-      username="Markotto23"
-    />
-    <TwitterCard
-      avatar="https://unavatar.io/github/johndoe"
-      name="John Doe"
-      username="johndoe"
-    />
-  </div>
-);
-
+      <TwitterCard
+        avatar="https://unavatar.io/github/mdo"
+        name="Mark Otto"
+        username="Markotto23"
+      />
+      <TwitterCard
+        avatar="https://unavatar.io/github/johndoe"
+        name="John Doe"
+        username="johndoe"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
